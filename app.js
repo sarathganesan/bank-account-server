9ed0c6a201a751c4ab0dbb681f1fa376dba245be
Sarathganesan3 <sarathganesan8@gmail.com>
2023-03-04 19:56:05 -0800

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fs = require('fs');
-
+const http = require('http');
 const app = express();
+const XLSX = require('xlsx');
 const PORT = 3000;
+const $ = require('jquery');
 
 class BankAccount {
   constructor(id, name, gender, dob, email, mobile, address, initialBalance, adharNo, panNo) {
@@ -69,59 +71,96 @@ class BankAccount {
     return transaction;
   }
 
-  getBalance() {
-    return this.initialBalance;
-  }
+ 
 
-  getTransactions() {
-    return this.transactions;
-  }
+getBalance() {
+  return this.initialBalance;
 }
+
+getTransactions() {
+  return this.transactions;
+}
+
+printStatement(res) {
+  // Create a new workbook and worksheet
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.json_to_sheet(this.transactions);
+
+  // Add the worksheet to the workbook
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Transactions');
+
+  // Generate the Excel file and save it to disk
+  const filePath = `${this.name}_statement.xlsx`;
+  XLSX.writeFile(workbook, filePath);
+
+  // Use res.sendFile to send the Excel file as a response
+  res.sendFile(filePath, { root: '.' }, (err) => {
+    if (err) {
+      console.error(`Error sending file: ${err}`);
+      res.status(500).send({ error: 'Unable to send file' });
+    } else {
+      console.log('File sent successfully');
+    }
+    // Delete the file from disk after sending the response
+    fs.unlinkSync(filePath);
+  });
+
+  return filePath;
+}
+
+
+
+}
+
+
 
 app.use(express.json());
 
 // create account
 app.post('/accounts', (req, res) => {
-    const { id, name, gender, dob, email, mobile, address, initialBalance, adharNo, panNo } = req.body[0] || {};
-  
-    if (!id || !name || !gender || !dob || !email || !mobile || !address || !initialBalance || !adharNo || !panNo) {
-      return res.status(400).json({ error: 'All fields are required' });
-    }
-  
-    const newAccount = new BankAccount(id, name, gender, dob, email, mobile, address, initialBalance, adharNo, panNo);
-  
-    const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
-    data.push(newAccount);
-    fs.writeFileSync('accounts.json', JSON.stringify(data));
-  
-    res.status(201).json(newAccount);
-  });
+  const { id, name, gender, dob, email, mobile, address, initialBalance, adharNo, panNo } = req.body[0] || {};
+
+  if (!id || !name || !gender || !dob || !email || !mobile || !address || !initialBalance || !adharNo || !panNo) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  const newAccount = new BankAccount(id, name, gender, dob, email, mobile, address, initialBalance, adharNo, panNo);
+
+  const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
+  data.push(newAccount);
+  fs.writeFileSync('accounts.json', JSON.stringify(data));
+
+  res.status(201).json(newAccount);
+});
 
 
-  //update kyc details
 
-  app.patch('/accounts/:id', (req, res) => {
-    const { name, dob, email, mobile, adharNo, panNo } = req.body;
-  
-    const accountId = req.params.id;
-  
-    const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
-    const account = data.find(acc => acc.id === accountId);
-  
-    if (!account) {
-      return res.status(404).json({ error: 'Account not found' });
-    }
-  
-    account.name = name || account.name;
-    account.dob = dob || account.dob;
-    account.email = email || account.email;
-    account.mobile = mobile || account.mobile;
-    account.adharNo = adharNo || account.adharNo;
-    account.panNo = panNo || account.panNo;
-  
-    fs.writeFileSync('accounts.json', JSON.stringify(data));
-  
-    res.status(200).json(account);
+
+
+//update kyc details
+
+app.patch('/accounts/:id', (req, res) => {
+  const { name, dob, email, mobile, adharNo, panNo } = req.body;
+
+  const accountId = req.params.id;
+
+  const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
+  const account = data.find(acc => acc.id === accountId);
+
+  if (!account) {
+    return res.status(404).json({ error: 'Account not found' });
+  }
+
+  account.name = name || account.name;
+  account.dob = dob || account.dob;
+  account.email = email || account.email;
+  account.mobile = mobile || account.mobile;
+  account.adharNo = adharNo || account.adharNo;
+  account.panNo = panNo || account.panNo;
+
+  fs.writeFileSync('accounts.json', JSON.stringify(data));
+
+  res.status(200).json(account);
 });
 
 //get account
@@ -138,178 +177,221 @@ app.get('/account/:id', (req, res) => {
   res.json(account);
 });
 
+
+
+
 //deposit money
 app.put('/account/:id/deposit', (req, res) => {
-    const { id } = req.params;
-    const { amount } = req.body;
-  
-    const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
-    const accountIndex = data.findIndex(acc => acc.id === id);
-  
-    if (accountIndex === -1) {
-      return res.status(404).json({ error: 'Account not found' });
-    }
-  
-    const accountData = data[accountIndex];
-    const account = new BankAccount(
-      accountData.id,
-      accountData.name,
-      accountData.gender,
-      accountData.dob,
-      accountData.email,
-      accountData.mobile,
-      accountData.address,
-      accountData.initialBalance,
-      accountData.adharNo,
-      accountData.panNo
-    );
-    account.deposit(amount);
-  
-    data[accountIndex] = account;
-    fs.writeFileSync('accounts.json', JSON.stringify(data));
-  
-    res.json({ message: 'Deposit successful' });
-  });
-  
-  //withdraw money
+  const { id } = req.params;
+  const { amount } = req.body;
 
-    app.put('/account/:id/withdraw', (req, res) => {
-    const { id } = req.params;
-    const { amount } = req.body;
+  const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
+  const accountIndex = data.findIndex(acc => acc.id === id);
   
-    const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
-    const accountIndex = data.findIndex(acc => acc.id === id);
-  
-    if (accountIndex === -1) {
-      return res.status(404).json({ error: 'Account not found' });
-    }
-  
-    const accountData = data[accountIndex];
-    const account = new BankAccount(
-      accountData.id,
-      accountData.name,
-      accountData.gender,
-      accountData.dob,
-      accountData.email,
-      accountData.mobile,
-      accountData.address,
-      accountData.initialBalance,
-      accountData.adharNo,
-      accountData.panNo
-    );
-  
-    try {
-      account.withdraw(amount);
-    } catch (error) {
-      return res.status(400).json({ error: error.message });
-    }
-  
-    data[accountIndex] = account;
-    fs.writeFileSync('accounts.json', JSON.stringify(data));
-  
-    res.json({ message: 'Withdrawal successful' });
+  if (accountIndex === -1) {
+    return res.status(404).json({ error: 'Account not found' });
+  }
+
+  const accountData = data[accountIndex];
+  const account = new BankAccount(
+    accountData.id,
+    accountData.name,
+    accountData.gender,
+    accountData.dob,
+    accountData.email,
+    accountData.mobile,
+    accountData.address,
+    accountData.initialBalance,
+    accountData.adharNo,
+    accountData.panNo
+  );
+  account.printStatement();
+
+  account.deposit(amount);
+
+  data[accountIndex] = account;
+  fs.writeFileSync('accounts.json', JSON.stringify(data));
+
+  res.json({ message: 'Deposit successful' });
 });
-  
+
+//withdraw money
+
+app.put('/account/:id/withdraw', (req, res) => {
+  const { id } = req.params;
+  const { amount } = req.body;
+
+  const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
+  const accountIndex = data.findIndex(acc => acc.id === id);
+
+  if (accountIndex === -1) {
+    return res.status(404).json({ error: 'Account not found' });
+  }
+
+  const accountData = data[accountIndex];
+  const account = new BankAccount(
+    accountData.id,
+    accountData.name,
+    accountData.gender,
+    accountData.dob,
+    accountData.email,
+    accountData.mobile,
+    accountData.address,
+    accountData.initialBalance,
+    accountData.adharNo,
+    accountData.panNo
+  );
+
+  try {
+    account.withdraw(amount);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  data[accountIndex] = account;
+  fs.writeFileSync('accounts.json', JSON.stringify(data));
+
+  res.json({ message: 'Withdrawal successful' });
+});
+
 // transfer money  
 app.post('/account/:id/transfer', (req, res) => {
-    const { id } = req.params;
-    const { toName, amount } = req.body;
-  
-    const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
-    const fromAccountIndex = data.findIndex(acc => acc.id === id);
-  
-    if (fromAccountIndex === -1) {
-      return res.status(404).json({ error: 'Account not found' });
-    }
-  
-    const fromAccountData = data[fromAccountIndex];
-    const fromAccount = new BankAccount(
-      fromAccountData.id,
-      fromAccountData.name,
-      fromAccountData.gender,
-      fromAccountData.dob,
-      fromAccountData.email,
-      fromAccountData.mobile,
-      fromAccountData.address,
-      fromAccountData.initialBalance,
-      fromAccountData.adharNo,
-      fromAccountData.panNo
-    );
-  
-    const toAccountIndex = data.findIndex(acc => acc.name === toName);
-  
-    if (toAccountIndex === -1) {
-      return res.status(404).json({ error: 'Destination account not found' });
-    }
-  
-    const toAccountData = data[toAccountIndex];
-    const toAccount = new BankAccount(
-      toAccountData.id,
-      toAccountData.name,
-      toAccountData.gender,
-      toAccountData.dob,
-      toAccountData.email,
-      toAccountData.mobile,
-      toAccountData.address,
-      toAccountData.initialBalance,
-      toAccountData.adharNo,
-      toAccountData.panNo
-    );
-  
-    try {
-      const transaction = fromAccount.transferMoney(toAccount.name, amount);
-      toAccount.receiveMoney(fromAccount.name, amount);
-  
-      data[fromAccountIndex] = fromAccount;
-      data[toAccountIndex] = toAccount;
-  
-      fs.writeFileSync('accounts.json', JSON.stringify(data));
-  
-      res.json({
-        message: 'Transfer successful',
-        transaction
-      });
-    } catch (error) {
-      return res.status(400).json({ error: error.message });
-    }
-  });
-  
-  
-  app.post('/account/:id/receive', (req, res) => {
-    const { id } = req.params;
-    const { fromName, amount } = req.body;
-  
-    const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
-    const toAccountIndex = data.findIndex(acc => acc.id === id);
-  
-    if (toAccountIndex === -1) {
-      return res.status(404).json({ error: 'Account not found' });
-    }
-  
-    const toAccount = data[toAccountIndex];
-    const fromAccountIndex = data.findIndex(acc => acc.name === fromName);
-  
-    if (fromAccountIndex === -1) {
-      return res.status(404).json({ error: 'Sender account not found' });
-    }
-  
-    const fromAccount = data[fromAccountIndex];
-    const transferResult = fromAccount.transferMoney(toAccount.name, amount);
-  
-    if (transferResult.error) {
-      return res.status(400).json({ error: transferResult.error });
-    }
-  
+  const { id } = req.params;
+  const { toName, amount } = req.body;
+
+  const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
+  const fromAccountIndex = data.findIndex(acc => acc.id === id);
+
+  if (fromAccountIndex === -1) {
+    return res.status(404).json({ error: 'Account not found' });
+  }
+
+  const fromAccountData = data[fromAccountIndex];
+  const fromAccount = new BankAccount(
+    fromAccountData.id,
+    fromAccountData.name,
+    fromAccountData.gender,
+    fromAccountData.dob,
+    fromAccountData.email,
+    fromAccountData.mobile,
+    fromAccountData.address,
+    fromAccountData.initialBalance,
+    fromAccountData.adharNo,
+    fromAccountData.panNo
+  );
+
+  const toAccountIndex = data.findIndex(acc => acc.name === toName);
+
+  if (toAccountIndex === -1) {
+    return res.status(404).json({ error: 'Destination account not found' });
+  }
+
+  const toAccountData = data[toAccountIndex];
+  const toAccount = new BankAccount(
+    toAccountData.id,
+    toAccountData.name,
+    toAccountData.gender,
+    toAccountData.dob,
+    toAccountData.email,
+    toAccountData.mobile,
+    toAccountData.address,
+    toAccountData.initialBalance,
+    toAccountData.adharNo,
+    toAccountData.panNo
+  );
+
+  try {
+    const transaction = fromAccount.transferMoney(toAccount.name, amount);
+    toAccount.receiveMoney(fromAccount.name, amount);
+
     data[fromAccountIndex] = fromAccount;
     data[toAccountIndex] = toAccount;
+
     fs.writeFileSync('accounts.json', JSON.stringify(data));
-  
-    res.json({ message: 'Received funds successfully' });
-  });
-  
-  
+
+    res.json({
+      message: 'Transfer successful',
+      transaction
+    });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
 
 
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-  });
+// receive money
+// receive money
+app.post('/account/:id/receive', (req, res) => {
+  const { id } = req.params;
+  const { fromName, amount } = req.body;
+
+  const data = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
+  const toAccountIndex = data.findIndex(acc => acc.id === id);
+
+  if (toAccountIndex === -1) {
+    return res.status(404).json({ error: 'Account not found' });
+  }
+
+  const toAccountData = data[toAccountIndex];
+  const toAccount = new BankAccount(
+    toAccountData.id,
+    toAccountData.name,
+    toAccountData.gender,
+    toAccountData.dob,
+    toAccountData.email,
+    toAccountData.mobile,
+    toAccountData.address,
+    toAccountData.initialBalance,
+    toAccountData.adharNo,
+    toAccountData.panNo
+  );
+
+  const fromAccountIndex = data.findIndex(acc => acc.name === fromName);
+
+  if (fromAccountIndex === -1) {
+    return res.status(404).json({ error: 'Sender account not found' });
+  }
+
+  const fromAccountData = data[fromAccountIndex];
+  const fromAccount = new BankAccount(
+    fromAccountData.id,
+    fromAccountData.name,
+    fromAccountData.gender,
+    fromAccountData.dob,
+    fromAccountData.email,
+    fromAccountData.mobile,
+    fromAccountData.address,
+    fromAccountData.initialBalance,
+    fromAccountData.adharNo,
+    fromAccountData.panNo
+  );
+
+  try {
+    const transaction = fromAccount.transferMoney(toAccount.name, amount);
+    toAccount.receiveMoney(fromAccount.name, amount);
+
+    data[fromAccountIndex] = fromAccount;
+    data[toAccountIndex] = toAccount;
+
+    fs.writeFileSync('accounts.json', JSON.stringify(data));
+
+    res.json({
+      message: 'Money received successfully',
+      transaction: {
+        type: 'receive',
+        amount,
+        fromName,
+        date: new Date()
+      }
+    });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
